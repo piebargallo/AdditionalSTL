43a73c889dd896e7a3c8fb9a46d196ea5d36428e
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-06-01 10:21:47 +0200

    Add files via upload

--- 01_Exer/01_Exer.cpp
+++ 01_Exer/01_Exer.cpp
@@ -1,54 +1,44 @@
-/* Chapter 14, "Handling Errors," explains error handling in C++ and explains that there are basically two major
-   options: either you work with error codes or your work with exceptions. I recommend using exceptions for error
-   handling, but for this exercise, you´ll use error codes. Write a simple Erroe class that just stores a single 
-   message, and has a getter to retrieve the message. Next write a getData() function with a single parameter 
-   called fail. If fail is false, the function retunrs a vector of some data; otherwise, it returns an instance of
-   Error. You are not allowed to use reference-to-non-const output parameters. Test your implementation in your
-   main() function. */
+/* Write an algorithm called transform_if(), similar to the Standard Library`s transform(), discussed in Chapter 20.
+   The difference is that transform_if() should accept an extra predicate and that only transforms elements for wich
+   the predicate returns true. Other elements are left untouched. to test your algorithm, create a vector of integers
+   and multiply all odd values in the vector by 2. */
 
 #include <iostream>
-#include <variant>
 #include <vector>
-#include <string>
+using std::cout;
+using std::endl;
+using std::vector;
 
-using namespace std;
-
-class Error
+template<typename InputIterator, typename OutputIterator,
+	typename Predicate, typename UnaryOperation>
+OutputIterator transform_if(InputIterator first, InputIterator last,
+	OutputIterator output, Predicate pred, UnaryOperation op)
 {
-public:
-	Error(string message) : m_message{ move(message) } { }
-	const string& getMessage() const { return m_message; }
-
-private:
-	string m_message;
-};
-
-variant<vector<int>, Error> getData(bool fail)
-{
-	if (fail) {
-		return Error{ "An error occured." };
-	}
-	else {
-		return vector{ 0, 1, 2, 3, 4 };
-	}
-}
-
-void handleResult(const variant<vector<int>, Error>& result)
-{
-	if (holds_alternative<Error>(result)) {
-		cout << "Error: " << get<Error>(result).getMessage() << endl;
-	}
-	else {
-		auto& data{ get<vector<int>>(result) };
-		for (auto& value : data) {
-			cout << value << " ";
+	auto iter{ first };
+	while (iter != last) {
+		if (pred(*iter)) {
+			*output = op(*iter);
 		}
-		cout << endl;
+		else {
+			*output = *iter;
+		}
+		++output;
+		++iter;
 	}
+	return output;
 }
 
 int main()
 {
-	handleResult(getData(false));
-	handleResult(getData(true));
+	vector values{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	for (auto& value : values) { cout << value << " "; }
+	cout << endl;
+
+	transform_if(begin(values), end(values), begin(values),
+		[](const auto& value) { return value % 2 != 0; },
+		[](const auto& value) { return value * 2; });
+
+	for (auto& value : values) { cout << value << " "; }
+	cout << endl;
 }
